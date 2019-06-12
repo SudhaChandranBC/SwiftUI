6659c53b2cf7aa55ca7ab1fda58064137b0eb62e
Chandran, Sudha <sudhachandran1bc@gmail.com>
2019-06-12 16:21:46 +0530

    Added posts code

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -10,25 +10,87 @@ import SwiftUI
 
 struct User: Identifiable {
     var id: Int
-    let username, message, imageName: String
+    let username, company, imageName, postTime, postText, postImageName: String
 }
 struct ContentView : View {
     let users: [User] = [
-        .init(id: 0, username: "Bill Gates", message: "Microsoft", imageName: "bill"),
-        .init(id: 1,username: "Tim Cook ", message: "Apple", imageName: "tim"),
-        .init(id: 2,username: "Sundar Pichai", message: "Pichai Sundararajan, also known as Sundar Pichai, is an Indian-American business executive. He is an engineer and the chief executive officer of Google LLC.", imageName: "sundar")
+        .init(id: 0, username: "Bill Gates", company: "Microsoft", imageName: "bill", postTime: "8h", postText:"Microsoft Corporation is an American multinational technology company with headquarters in Redmond, Washington. ", postImageName: "microsoft"),
+        .init(id: 1,username: "Tim Cook ", company: "Apple", imageName: "tim", postTime: "2h", postText:"Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.", postImageName:"apple"),
+        .init(id: 2,username: "Sundar Pichai", company: "Microsoft", imageName: "sundar", postTime: "6h", postText:"Google LLC is an American multinational technology company that specializes in Internet-related services and products.", postImageName:"google")
     ]
     
     var body: some View {
         NavigationView {
             List {
-                Text("Header").font(.largeTitle)
+                ScrollView {
+                    Text("Trending users..")
+                    HStack {
+                        ForEach(users.identified(by: \.id)) { user in
+                            TrendingUser(user: user)
+                            TrendingUser(user: user)
+                        }
+                    }
+                }.frame(height: 150)
+                
+                Text("Users Posts...").font(.largeTitle)
                 ForEach(users.identified(by: \.id)) { user in
                     //User row
-                    UserRow(user: user)
+                    UserPost(user: user)
+                    
                 }
-            }.navigationBarTitle(Text("Dynamic List"))
+            }.navigationBarTitle(Text("Dynamic User List"))
         }
+    }
+}
+
+struct DetailedView: View {
+    var body: some View {
+        Text("Detail View")
+    }
+}
+
+struct UserPost: View {
+    let user: User
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            HStack {
+                Image(user.imageName)
+                    .resizable()
+                    .frame(width: 70, height: 70)
+                    .clipShape(Circle())
+                    .clipped()
+                VStack(alignment: .leading) {
+                    Text(user.username).font(.headline)
+                    Text(user.postTime)
+                    }.padding(.leading, 8)
+                }.padding(.leading, 16).padding(.top, 16)
+            
+            Text(user.postText)
+                .lineLimit(nil)
+                .padding(.leading, 16)
+                .padding(.trailing, 100)
+            Image(user.postImageName)
+                .frame(height: 350)
+                .clipped()
+        }.padding(.leading, -20)
+            .padding(.trailing, -20)
+            .padding(.bottom, 0)
+        
+    }
+}
+
+struct TrendingUser: View {
+    let user: User
+    var body: some View {
+        VStack (alignment: .leading) {
+            Image(user.imageName)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .clipped()
+            Text(user.username).font(.subheadline)
+            Text(user.company).font(.caption)
+
+            }.padding()
     }
 }
 
@@ -38,13 +100,13 @@ struct UserRow: View {
         HStack {
             Image(user.imageName)
                 .resizable()
-                .frame(width: 70, height: 70)
+                .frame(width: 60, height: 60)
                 .clipShape(Circle())
                 .overlay(Circle().stroke(Color.gray, lineWidth: 1))
                 .shadow(radius: 0.5)
             VStack(alignment: .leading) {
                 Text(user.username).font(.headline)
-                Text(user.message).lineLimit(nil)
+                Text(user.postText).lineLimit(nil)
                 }.padding(.leading, 8)
             }.padding()
     }
